336565162014fa013765954272c69e3aaeeed2b1
MariolyTintaya <tintayaramosmarioly@gmail.com>
2025-05-04 23:54:47 -0400

    Adicionando Dashboard de tutor

--- apps/tutores/urls.py
+++ apps/tutores/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from apps.tutores.views import dashboard_tutor
 urlpatterns = [
     path('listar/', views.listar_tutores, name='listar_tutores'),
+    path('dashboard/tutor/', views.dashboard_tutor, name='dashboard_tutor'),
+
 ]
--- apps/tutores/views.py
+++ apps/tutores/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from .models import Tutor
+from django.db import connection
 
 def listar_tutores(request):
     tutores = Tutor.objects.all()
     return render(request, 'tutores/listar_tutores.html', {'tutores': tutores})
+
+def dashboard_tutor(request):
+    id_usuario = request.session.get('id_usuario')
+
+    with connection.cursor() as cursor:
+        # Obtener datos del tutor
+        cursor.execute("""
+            SELECT t.nombres, t.apellidos, t.correo
+            FROM tutor t
+            WHERE t.id_usuario = %s
+        """, [id_usuario])
+        tutor = cursor.fetchone()
+
+        # Obtener estudiantes asociados
+        cursor.execute("""
+            SELECT e.nombres, e.apellidos, e.id
+            FROM tiene_tutor tt
+            JOIN estudiante e ON tt.id_estudiante = e.id
+            JOIN tutor t ON tt.id_tutor = t.id
+            WHERE t.id_usuario = %s
+        """, [id_usuario])
+        estudiantes = cursor.fetchall()
+
+    context = {
+        'tutor': tutor,
+        'estudiantes': estudiantes
+    }
+    return render(request, 'dashboard_tutor.html', context)
